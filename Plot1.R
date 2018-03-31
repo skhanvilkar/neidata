e18e22289ca668c1c539e712ee59ad74b9b8cb0f
skhanvilkar <35381954+skhanvilkar@users.noreply.github.com>
2018-03-30 17:15:16 -0700

    Add files via upload

--- Plot1.R
+++ Plot1.R
@@ -9,7 +9,7 @@ emitdata <- with(NEI, aggregate(Emissions, by = list(year), sum))
 q1 <- subset(NEI, fips == "24510")
 balt <- tapply(q1$Emissions, q1$year, sum)
 plot(balt, type = "o", main = "Total PM2.5 Emissions in Baltimore County", xlab = "Year", ylab = "PM2.5 Emissions", pch = 19, col = "blue", lty = 5)
-dev.copy(png, file = "C:/Users/skhanvilkar/Documents/neidata/plot2.png") 
+dev.copy(png, file = "C:/Users/skhanvilkar/Documents/neidata/plot1.png") 
 dev.off()
 
 
